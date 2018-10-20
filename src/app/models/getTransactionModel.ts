317736e2b1c052ff7ddae018a20ac7a4e5e52b91
Max Mozgovoj <maxmozgovoj13696@gmail.com>
2018-10-20 18:20:42 +0300

    Deleted files

--- src/app/models/getTransactionModel.ts
+++
@@ -1,9 +0,0 @@
-export interface IGetTansactionModel {
-  accountname: string;
-  token: string;
-  spot_id: Number;
-  spot_tablet_id: Number;
-  table_id: Number;
-  user_id: Number;
-  guests_count: Number;
-}
