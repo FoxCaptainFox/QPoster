200e75d39f90664ec8e240fe66dec5fe1bbc69f3
Max Mozgovoj <maxmozgovoj13696@gmail.com>
2018-10-20 17:56:10 +0300

    Fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
 import { StartPageModule } from './layouts/start-page/start-page.module';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MainComponent } from './layouts/main/main.component';
 import { AboutComponent } from './layouts/about/about.component';
 
