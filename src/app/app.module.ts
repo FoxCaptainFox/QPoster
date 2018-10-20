31ad1177269c28399fe8dd1e92a8c7cc43087171
Ivan Hladkiy <liberte1793@gmail.com>
2018-10-20 17:28:47 +0300

    added about screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './layouts/main/main.component';
+import { AboutComponent } from './layouts/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    HttpClientModule
   ],
-  providers: [],
+  exports: [
+    MainComponent
+  ],
+  providers: [HttpClientModule, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AboutService } from '../../services/about-service.service';
+import { ICompanyNameOrLogo } from '../../models/ICompanyNameOrLogo'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +12,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  companyName : Observable<ICompanyNameOrLogo>;
+
+  constructor(private aboutService: AboutService) { }
 
   ngOnInit() {
+    this.companyName = this.aboutService.getCompanyName().pipe(
+      map((x: any) => x.response as ICompanyNameOrLogo)
+    );
   }
 
 }
