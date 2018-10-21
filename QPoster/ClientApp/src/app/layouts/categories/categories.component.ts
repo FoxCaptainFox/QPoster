283b29efdf4bcf1ba99346ea6844388aeaa264fd
Max Mozgovoj <maxmozgovoj13696@gmail.com>
2018-10-21 13:31:45 +0300

    fixed back button

--- QPoster/ClientApp/src/app/layouts/categories/categories.component.ts
+++ QPoster/ClientApp/src/app/layouts/categories/categories.component.ts
@@ -4,6 +4,7 @@ import { IProduct } from '../../models/IProduct';
 import { Observable } from 'rxjs';
 import { MenuService } from '../../services/menu.service';
 import { map } from 'rxjs/operators';
+import { CategoriesService } from 'src/app/services/local/categories.service';
 
 
 @Component({
@@ -16,29 +17,35 @@ export class CategoriesComponent implements OnInit {
   @Input() companyName: string;
 
   isBtnVisible = false;
-  categories: Observable<ICategory[]>; 
-  products: Observable<IProduct[]>; 
+  categories: Observable<ICategory[]>;
+  products: Observable<IProduct[]>;
   parentCategory = null;
   category_id;
 
-  constructor(private menuService: MenuService) { }
+  constructor(private menuService: MenuService,
+    private categoriesService: CategoriesService) { }
 
   ngOnInit() {
-    this.getDefaultMenu()
+    this.getDefaultMenu();
+
+    this.categoriesService.clickEvent.subscribe(() => {
+      this.categoriesService.setButtonVisibility(false);
+      this.getDefaultMenu();
+    });
   }
 
-  getDefaultMenu(){
+  getDefaultMenu() {
     this.getCategories();
     this.getProducts(0);
   }
 
-  getProductsInCategory(category_id){
-    this.isBtnVisible = true;
+  getProductsInCategory(category_id) {
+    this.categoriesService.setButtonVisibility(true);
     this.categories = new Observable<ICategory[]>();
     this.getProducts(category_id);
   }
 
-  getProducts(category_id){
+  getProducts(category_id) {
     this.products = this.menuService.getProducts(category_id).pipe(
       map((result: any) => {
         return result.response as IProduct[];
@@ -48,18 +55,13 @@ export class CategoriesComponent implements OnInit {
     this.products.subscribe();
   }
 
-  getCategories(){
+  getCategories() {
     this.categories = this.menuService.getCategories().pipe(
       map((result: any) => {
-        return result.response as ICategory[]
+        return result.response as ICategory[];
       })
     );
 
     this.categories.subscribe();
-  }
-
-  onBtnClick(){
-    this.isBtnVisible = false;
-    this.getDefaultMenu()
   }
 }
